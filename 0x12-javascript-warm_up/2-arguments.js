29bc1525e9c7e34f985518e687661822b04a56bc
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-06 04:50:10 +0300

    quick fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-import { argv } from 'node:process';
-
 if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
@@ -9,4 +6,3 @@ if (process.argv.length < 3) {
 } else {
   console.log('Arguments found');
 }
-
